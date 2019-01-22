e13cdb9a7effc7ab06153ef8dcb93f9772c22745
DemonLee <libingxue4578@gmail.com>
2019-01-22 23:12:16 +0800

    My change

--- src/App.js
+++ src/App.js
@@ -17,12 +17,13 @@ class App extends Component {
         return (
           <div>
               <div className={"envelope animated "+ this.state.class}>
+                <div className="text">亲爱的，你收到一封来信！</div>
                 <div className="triangle-down"></div>
                   <img className="heart" src={iconurl} onClick={()=>{
                       this.setState({class:'bounceOutRight',show:true})
                   }}/>
                   <div className="triangle-up"></div>
-                  <div className="text">你收到一封信，点击查收！</div>
+
               </div>
               {main()}
           </div>
